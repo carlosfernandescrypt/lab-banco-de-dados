41ab8aab4c27a9dbf70d5f0df7ae24a349e493cd
vinicius lacerda de souza <viniciuslacerda437@gmail.com>
2024-11-27 19:49:57 -0300

    Create inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,6 @@
+INSERT INTO Usuarios (nome_usuario, email, data_nascimento, foto_perfil)
+VALUES
+('joao123', 'joao123@email.com', '1990-05-15', 'foto1.png'),
+('maria456', 'maria456@email.com', '1985-03-20', 'foto2.png'),
+('carlos789', 'carlos789@email.com', '2000-10-05', 'foto3.png'),
+('ana321', 'ana321@email.com', '1995-07-12', 'foto4.png');
