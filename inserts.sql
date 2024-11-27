4556c6b609c2b639f46b3cf7be68e8516a94862f
vinicius lacerda de souza <viniciuslacerda437@gmail.com>
2024-11-27 19:50:38 -0300

    Update inserts.sql

--- inserts.sql
+++ inserts.sql
@@ -4,3 +4,10 @@ VALUES
 ('maria456', 'maria456@email.com', '1985-03-20', 'foto2.png'),
 ('carlos789', 'carlos789@email.com', '2000-10-05', 'foto3.png'),
 ('ana321', 'ana321@email.com', '1995-07-12', 'foto4.png');
+
+INSERT INTO Conexoes (id_usuario1, id_usuario2, data_criacao)
+VALUES
+(1, 2, NOW()),
+(2, 3, NOW()),
+(3, 4, NOW()),
+(4, 1, NOW());
