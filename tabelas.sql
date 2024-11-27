bf02eb314509e9c7845401ef04525a010097df08
vitinjo <135727142+vitinjo@users.noreply.github.com>
2024-11-27 20:13:10 -0300

    Update tabelas.sql

--- tabelas.sql
+++ tabelas.sql
@@ -5,3 +5,12 @@ CREATE TABLE Usuarios (
   data_nascimento DATE NOT NULL,
   foto_perfil VARCHAR(255)
 );
+
+CREATE TABLE Conexoes (
+  id_conexao INT AUTO_INCREMENT PRIMARY KEY,
+  id_usuario1 INT NOT NULL,
+  id_usuario2 INT NOT NULL,
+  data_criacao DATETIME NOT NULL,
+  FOREIGN KEY (id_usuario1) REFERENCES Usuarios(id_usuario),
+  FOREIGN KEY (id_usuario2) REFERENCES Usuarios(id_usuario)
+);
