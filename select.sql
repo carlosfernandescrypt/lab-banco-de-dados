de9051bf3e4d8aa6d3cdf06e5fb88822c50f8259
werley102 <141765055+werley102@users.noreply.github.com>
2024-11-27 20:16:01 -0300

    Ver membros de um grupo

--- select.sql
+++ select.sql
@@ -9,3 +9,8 @@ SELECT n.tipo, n.data_hora, u.nome_usuario AS origem
 FROM Notificacoes n
 JOIN Usuarios u ON n.origem_id = u.id_usuario
 WHERE n.id_usuario = 1;
+
+SELECT m.funcao, u.nome_usuario
+FROM membrosGrupos m
+JOIN Usuarios u ON m.id_usuario = u.id_usuario
+WHERE m.id_grupo = 1;
