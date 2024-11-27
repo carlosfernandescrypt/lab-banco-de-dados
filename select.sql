899050a16c18f3032a327df5131c44de780c7365
werley102 <141765055+werley102@users.noreply.github.com>
2024-11-27 20:15:36 -0300

    Ver notificações de um usuário

--- select.sql
+++ select.sql
@@ -4,3 +4,8 @@ SELECT c.conteudo, u.nome_usuario
 FROM Comentarios c
 JOIN Usuarios u ON c.id_usuario = u.id_usuario
 WHERE c.id_postagem = 1;
+
+SELECT n.tipo, n.data_hora, u.nome_usuario AS origem
+FROM Notificacoes n
+JOIN Usuarios u ON n.origem_id = u.id_usuario
+WHERE n.id_usuario = 1;
